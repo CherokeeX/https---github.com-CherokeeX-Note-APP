9dc8d149ae40323449cc105c6dd8b7a332f70e6a
CherokeeX <serefb80@gmail.com>
2023-03-15 01:08:15 +0300

    sytling over

--- src/companents/NoteLists.js
+++ src/companents/NoteLists.js
@@ -2,19 +2,19 @@ import Note from "./Note";
 
 const NoteLists = ({ notes ,removeNote }) => {
 
-    return (
+    return notes.length > 0 ? 
+    (
+        <div className="note-list">
+            <ul>
+                { notes.map(note => { return (<Note note={note} key={ note.id } removeNote={removeNote} />); }) }
+            </ul>
+        </div>
+    ) : 
+    (
+        <div className="empty"> <h2>There is no Any Note Yet</h2>  </div>
+    );
 
-    <div className="note-list">    
-         <ul className="title">
-             { notes.map( note => {
-              return (<Note note={note} key={note.id} removeNote = {removeNote}/>
-            );
-        }) }
-        </ul>
-    </div>
-            );
-    
-        }
+}
 
 
 export default NoteLists;
